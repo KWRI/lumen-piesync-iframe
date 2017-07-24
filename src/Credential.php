22225cfde7cbadc50f68f9c544b09def99c3fb66
Nurcahyo al Hidayah <2light.hidayah@gmail.com>
2017-07-24 19:11:05 +0700

    Add Credential model serializer

---
+++ src/Credential.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Piesync\Partner;
+use JsonSerializable;
+use Serializable;
+use Illuminate\Contracts\Support\Arrayable;
+
+class Credential implements JsonSerializable, Arrayable, Serializable
+{
+    public $publicPemFile;
+    public $privatePemFile;
+
+    public function jsonSerialize()
+    {
+        return $this->toArray();
+    }
+
+    public function serialize() {
+        return json_encode($this->toArray());
+    }
+
+    public function unserialize($data) {
+        $unserialized = json_decode($data, true);
+        $this->publicPemFile = $unserialized['public_pem_file'];
+        $this->privatePemFile = $unserialized['private_pem_file'];
+    }
+
+    public function toArray()
+    {
+        return [
+            'public_pem_file' => $this->publicPemFile,
+            'private_pem_file' => $this->privatePemFile,
+        ];
+    }
+
+}
