e1dba1b5326b301a6be0dfa24ae278c4654b11ba
Nurcahyo al Hidayah <2light.hidayah@gmail.com>
2017-07-24 19:10:52 +0700

    Add generate pem file on PartnerTokenGenerator

--- src/Console/PartnerTokenGenerator.php
+++ src/Console/PartnerTokenGenerator.php
@@ -3,9 +3,107 @@
 namespace Piesync\Partner\Console;
 
 use Illuminate\Console\Command;
-
+use Illuminate\Filesystem\FileSystem;
+use Piesync\Partner\Credential;
+use Piesync\Partner\OpenSSL;
 
 class PartnerTokenGenerator extends Command
 {
+
+    protected $description = "Generate piesync partner API token";
+
+    protected $signature = 'make:piesync-token {--pem-dir=} {--payload-file=}';
+
+    protected $pemDir;
+
+    protected $credential;
+
+    protected $filesystem;
+
+    protected $openSSL;
+
+    protected $pemResource;
+
+    protected $credentialPath;
+
+    public function __construct(Filesystem $filesystem, Credential $credential, OpenSSL $openSSL)
+    {
+        parent::__construct();
+        $this->filesystem = $filesystem;
+        $this->credential = $credential;
+        $this->openSSL = $openSSL;
+    }
+
+    public function fire()
+    {
+        $this->credentialPath = $this->laravel->basePath('/private/piesync_secret.json');
+        $this->pemDir = $this->option('pem-dir') ?: $this->laravel->basePath(). '/private';
+        $this->pemDir = realpath($this->pemDir);
+        if (!$this->checkCredential()) {
+            $this->ensureDirectory($this->pemDir);
+            $this->generatePem();
+            $this->createPrivatePemFile();
+            $this->createPublicPemFile();
+            $this->compileCredential();
+            $this->info('it works!');
+        }
+
+        $this->generateToken();
+    }
+
+    private function checkCredential()
+    {
+        if (!$this->filesystem->exists($this->credentialPath)) {
+            return false;
+        };
+
+        $credential = $this->filesystem->get($this->credentialPath);
+        $this->credential->unserialize($credential);
+        return true;
+    }
+
+
+    private function ensureDirectory($directory)
+    {
+
+        if (!$this->filesystem->exists($directory)) {
+            $this->filesystem->makeDirectory($directory, 0755, true);
+        }
+    }
+
+    private function generatePem()
+    {
+        $this->pemResource = $this->openSSL->genRSA(2048);
+    }
+
+    private function createPrivatePemFile()
+    {
+        $pemFile = "{$this->pemDir}/piesync_private.pem";
+        $this->info("Create Pem File on {$pemFile}");
+
+        $this->filesystem->put($pemFile, $this->openSSL->getPrivateKey($this->pemResource));
+        $this->credential->privatePemFile = $pemFile;
+
+    }
+
+    private function createPublicPemFile()
+    {
+        $pemFile = "{$this->pemDir}/piesync_public.pem";
+        $this->info("Create public pem file on $pemFile");
+        $this->filesystem->put($pemFile, $this->openSSL->getPublicKey($this->pemResource));
+        $this->credential->publicPemFile = $pemFile;
+    }
+
+    private function compileCredential()
+    {
+
+        $this->ensureDirectory(dirname($this->credentialPath));
+        $this->filesystem->put($this->credentialPath, $this->credential->serialize());
+    }
+
+    private function generateToken()
+    {
+        
+    }
 
 }
